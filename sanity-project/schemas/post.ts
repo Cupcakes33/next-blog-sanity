5fc54f6522c94c0759916c89655aab9a3050ca46
Cupcakes33 <rute930627@gmail.com>
2023-07-27 18:38:21 +0900

    :memo: Docs : change post schema(add tag, category)

--- sanity-project/schemas/post.ts
+++ sanity-project/schemas/post.ts
@@ -14,6 +14,25 @@ export default {
       title: 'Overview',
     },
     {
+      name: 'category',
+      type: 'string',
+      title: 'Category',
+      options: {
+        list: [
+          {title: 'Frontend', value: 'Frontend'},
+          {title: 'Library', value: 'Library'},
+          {title: 'Other', value: 'Other'},
+        ],
+        layout: 'dropdown',
+      },
+    },
+    {
+      name: 'tag',
+      type: 'array',
+      title: 'Tag',
+      of: [{type: 'string'}],
+    },
+    {
       name: 'slug',
       type: 'slug',
       title: 'Slug',
